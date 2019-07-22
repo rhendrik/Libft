2b3be3e2ff402868505912931b4e6d7692926154
Ri-Hani HENDRIKS <rhendrik@c4r19s2.wethinkcode.co.za>
2019-07-22 08:28:28 +0200

    some normies

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhendrik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/22 08:20:20 by rhendrik          #+#    #+#             */
+/*   Updated: 2019/07/22 08:21:42 by rhendrik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void ft_lstdel(t_list **alist,void (*del)(void *, size_t))
+void	ft_lstdel(t_list **alist, void (*del)(void *, size_t))
 {
-	if((*alist)->next)
+	if ((*alist)->next)
 	{
 		ft_lstdel(&(*alist), del);
 	}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rhendrik <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 11:00:48 by rhendrik          #+#    #+#             */
-/*   Updated: 2019/07/16 12:33:20 by rhendrik         ###   ########.fr       */
+/*   Updated: 2019/07/22 08:26:51 by rhendrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@
 #define MINSTR "-2147483648"
 #define MAXSTR "2147483647"
 
-typedef struct s_list
+typedef struct		s_list
 {
-	void *content;
-	size_t content_size;
-	struct s_list *next;
-} t_list;
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
 void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
-void ft_lstdel(t_list **alst, void (*del)(void *,size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 unsigned int		smin(const char s1, const char s2);
 t_list				*ft_lstnew(const void *content, size_t content_size);
 void				*ft_memset(void *s, int c, size_t n);
@@ -54,7 +54,7 @@ int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_atoi(const char *nptr);
 char				*ft_strnstr(const char *haystack,
-const char 			*needle, size_t n);
+const char			*needle, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
@@ -87,6 +87,5 @@ char				**ft_strsplit(const char *s, char c);
 size_t				ft_strlcat(char *dest, const char *src, size_t size);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memrcpy(void *dest, const void *src, size_t n);
-
 
 #	endif
